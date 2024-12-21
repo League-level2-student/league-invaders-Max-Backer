3e435c8ece554cc4a8a5d3df81d35c75e8ee9f22
Max-Backer <max.r.backer@gmail.com>
2024-12-21 11:26:27 -0800

    max

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,22 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	GamePanel gamePanel;
+	JFrame frame;
+	public static int WIDTH = 600;
+	public static int HEIGHT = 800;
 
+	
+
+	public LeagueInvaders() {
+		gamePanel = new GamePanel();
+		frame = new JFrame();
+	}
+
+	public void setup() {
+		frame.add(gamePanel);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(WIDTH, HEIGHT);
+	}
 }
---
+++ src/LeagueInvadersRunner.java
@@ -0,0 +1,10 @@
+
+public class LeagueInvadersRunner {
+
+	public static void main(String[] args) {
+		LeagueInvaders invaders = new LeagueInvaders();
+		invaders.setup();
+
+	}
+
+}
